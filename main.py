94ac456f45413de5e0cc87d6a2256dc19a1508e4
AdlerHu <55276788+AdlerHu@users.noreply.github.com>
2020-03-26 13:04:16 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,129 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import time
+import random
+import MySQLdb
+import os
+
+path = './jobs/'
+if not os.path.exists(path):
+    os.mkdir(path)
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/80.0.3987.132 Safari/537.36'}
+
+keyword = '資料庫'
+work_dict_head = 'https://www.104.com.tw/job/ajax/content/'
+
+
+ss = requests.session()
+
+# The title of each job
+vacancy_title = []
+
+# The url of each job
+vacancy_urls = []
+
+# Require Skills
+skills = []
+
+# Salary
+salarys = []
+
+# Job Description
+job_descriptions = []
+
+# Company Name
+company_names = []
+
+# Company Page
+company_pages = []
+
+# Address
+addresses = []
+
+# The variable of work-context dictionary
+work_dict_urls = []
+
+for page in range(1, 6):
+    url = 'https://www.104.com.tw/jobs/search/?ro=0&keyword=' \
+          + keyword + '&order=15&asc=0&page=' + str(page) + '&mode=s'
+
+    res = ss.get(url, headers=headers)
+    soup = BeautifulSoup(res.text, 'html.parser')
+
+    # To get the vacancy blocks
+    vacancys = soup.select('div.b-block__left')
+
+    # To get the title and url for each vacancy
+    for vacancy in vacancys:
+
+        tmp_a = vacancy.select('a.js-job-link')
+        for a in tmp_a:
+            vacancy_title.append(a.text)
+            vacancy_urls.append('https:' + a['href'])
+            work_dict_urls.append(work_dict_head + a['href'][21:26])
+
+    # get into each work_dict
+    for work_dict_url in work_dict_urls:
+        res = ss.get(work_dict_url, headers=headers)
+        job_content = json.loads(res.text)
+
+        skill_dict = job_content['data']['condition']['specialty']
+        skills_list = []
+
+        # from skill dict get skills
+        for skill in skill_dict:
+            skills_list.append(skill['description'])
+            skills.append(skills_list)
+
+        job_name = job_content['data']['header']['jobName']
+        salary = job_content['data']['jobDetail']['salary']
+        job_description = job_content['data']['jobDetail']['jobDescription']
+        company_name = job_content['data']['header']['custName']
+        company_page = job_content['data']['header']['custUrl']
+        address = job_content['data']['jobDetail']['addressRegion'] + job_content['data']['jobDetail']['addressDetail']
+
+        # salarys.append(job_content['data']['jobDetail']['salary'])
+        #
+        # job_descriptions.append(job_content['data']['jobDetail']['jobDescription'])
+        #
+        # company_names.append(job_content['data']['header']['custName'])
+        #
+        # company_pages.append(job_content['data']['header']['custUrl'])
+        #
+        # addresses.append(job_content['data']['jobDetail']['addressRegion']
+        #                  + job_content['data']['jobDetail']['addressDetail'])
+
+        print(job_name)
+        print(salary)
+        print(job_description)
+        print(company_name)
+        print(company_page)
+        print(address)
+        print(skills_list)
+        print('-------------------------------------------------------------------')
+
+        file_content = ''
+
+        file_content += job_name + '\n'
+        file_content += salary + '\n'
+        file_content += job_description + '\n'
+        file_content += company_name + '\n'
+        file_content += company_page + '\n'
+        file_content += address + '\n'
+
+        for skill in skills_list:
+            file_content += skill + ','
+
+        try:
+            with open(path + company_name + '-' + job_name, 'w', encoding='utf-8') as f:
+                f.write(file_content)
+        except Exception as err:
+            print(err.args)
+
+        time.sleep(random.randint(0, 2))
+
+    print('換頁換頁換頁換頁換頁換頁換頁換頁換頁換頁換頁換頁換頁換頁換頁換頁')
