0c944d0e7e7f7f529eab030c794bfd435ebba172
AdlerHu <55276788+AdlerHu@users.noreply.github.com>
2020-04-02 20:09:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,27 +16,21 @@ headers = {
 keyword = '資料庫'
 work_dict_head = 'https://www.104.com.tw/job/ajax/content/'
 
-
 ss = requests.session()
-
-# The title of each job
-vacancy_title = []
-
-# The url of each job
-vacancy_urls = []
-
-# Require Skills
-skills = []
 
 # The variable of work-context dictionary
 work_dict_urls = []
 
-for page in range(1, 51):
+for page in range(1, 101):
     url = 'https://www.104.com.tw/jobs/search/?ro=0&keyword=' \
           + keyword + '&order=15&asc=0&page=' + str(page) + '&mode=s'
 
     res = ss.get(url, headers=headers)
     soup = BeautifulSoup(res.text, 'html.parser')
+
+    # A dictionary about information of the vacancy
+    vacancy_dict = {'job_name': '', 'company_name': '', 'salary': '', 'job_description': '',
+                    'company_page': '', 'address': '', 'link': '', 'skills': ''}
 
     # To get the vacancy blocks
     vacancys = soup.select('div.b-block__left')
@@ -46,8 +40,7 @@ for page in range(1, 51):
 
         tmp_a = vacancy.select('a.js-job-link')
         for a in tmp_a:
-            vacancy_title.append(a.text)
-            vacancy_urls.append('https:' + a['href'])
+            vacancy_dict['link'] = 'https:' + a['href']
             work_dict_urls.append(work_dict_head + a['href'][21:26])
 
     # get into each work_dict
@@ -55,49 +48,36 @@ for page in range(1, 51):
         res = ss.get(work_dict_url, headers=headers)
         job_content = json.loads(res.text)
 
-        skills_list = []
-        job_name = ''
-        salary = ''
-        job_description = ''
-        company_name = ''
-        company_page = ''
-        address = ''
-        
+        skills_str = ''
+
         try:
             skill_dict = job_content['data']['condition']['specialty']
 
             # from skill dict get skills
             for skill in skill_dict:
-                skills_list.append(skill['description'])
-                skills.append(skills_list)
+                skills_str += skill['description'] + ','
 
-            job_name = job_content['data']['header']['jobName']
-            salary = job_content['data']['jobDetail']['salary']
-            job_description = job_content['data']['jobDetail']['jobDescription']
-            company_name = job_content['data']['header']['custName']
-            company_page = job_content['data']['header']['custUrl']
-            address = job_content['data']['jobDetail']['addressRegion'] + job_content['data']['jobDetail']['addressDetail']
+            vacancy_dict['job_name'] = job_content['data']['header']['jobName']
+            vacancy_dict['salary'] = job_content['data']['jobDetail']['salary']
+            vacancy_dict['job_description'] = job_content['data']['jobDetail']['jobDescription']
+            vacancy_dict['company_name'] = job_content['data']['header']['custName']
+            vacancy_dict['company_page'] = job_content['data']['header']['custUrl']
+            vacancy_dict['address'] = job_content['data']['jobDetail']['addressRegion'] +\
+                                      job_content['data']['jobDetail']['addressDetail']
+            vacancy_dict['skills'] = skills_str
+
         except Exception as err:
             print(err.args)
             continue
 
         print('Done')
 
-        file_content = ''
-
-        file_content += job_name + '\n'
-        file_content += salary + '\n'
-        file_content += job_description + '\n'
-        file_content += company_name + '\n'
-        file_content += company_page + '\n'
-        file_content += address + '\n'
-
-        for skill in skills_list:
-            file_content += skill + ','
-
         try:
-            with open(path + company_name + '-' + job_name + '.txt', 'w', encoding='utf-8') as f:
-                f.write(file_content)
+            with open(path + vacancy_dict['company_name'] + '-' + vacancy_dict['job_name'] + '.txt', 'w',
+                      encoding='utf-8') as f:
+                for key in vacancy_dict:
+                    f.writelines(vacancy_dict[key] + '\n')
+
         except Exception as err:
             print(err.args)
 
